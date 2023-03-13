acafbcba620fa15d2892b5674315332037593825
OH_WOW <s.a_ln@mail.ru>
2023-03-13 10:16:41 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+// double[,] a = new double[5, 10];
+// Random random = new Random();
+// for (int i = 0; i < 5; i++)
+// {
+//     for (int j = 0; j < 10; j++)
+//     {
+//         a[i, j] = random.NextDouble() * 100; // случайное вещественное число в диапазоне от 0 до 1
+//         Console.Write("{0,6:F2}", a[i, j]);
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// Console.WriteLine("Введите размер массива");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] array = new int[m, n];
+ 
+// for (int i = 0; i < array.GetLength(0); i++) 
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//         array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+// }
+ 
+// for (int i = 0; i < array.GetLength(0); i++) 
+// {
+//     for (int j = 0; j < array.GetLength(1); j++)
+//         Console.Write(array[i,j] + "\t  ");
+//         Console.WriteLine();
+// }
+ 
+//  Console.WriteLine("Введите координаты");
+//  int a = Convert.ToInt32(Console.ReadLine());
+//  int b = Convert.ToInt32(Console.ReadLine());
+//  if (a>m && b>n)
+//  Console.WriteLine("такого числа нет");
+//  else
+//  {
+//  object c = array.GetValue(a,b);
+//  Console.WriteLine(c);
+//  }
+
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// Random random = new Random();
+// int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i, j] = random.Next(1, 10);
+//         Console.Write(arr[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine("---------------------------");
+// Console.WriteLine(arr.GetLength(0));
+// for (int j = 0; j < arr.GetLength(1); j++)
+// {
+//     double sum = 0;
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         sum += arr[i, j];
+//     }
+//     Console.Write($"{ sum / arr.GetLength(0)} ");
+// }
+// Console.ReadLine();
